d71f6148c24c9bdbd026e1c49b8720f22ff53597
Alexander Zaretskii <al.zaretskii@gmail.com>
2015-12-03 14:22:44 +0200

    add DEXTER module

--- fixtures/default.js
+++ fixtures/default.js
@@ -63,7 +63,8 @@ module.exports = _.merge({
              */
             input: {
                 //Replace VAR1 with the name of an expected input, and add more inputs as needed.
-                VAR1: 'foo'
+                id: '43ZHCT0cAZBISjO8DG9PnE',
+                country: 'SE'
             }
         }
     }
